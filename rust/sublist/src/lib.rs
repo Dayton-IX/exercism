0f08276ad78305340489cf6fcc28a4c8b83728aa
Dayton-IX <max.dayton@protonmail.com>
2022-11-04 13:44:31 -0600

    wrong approach

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,5 +7,27 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    let mut result = Comparison::Unequal;
+    let mut i = 0;
+    if _first_list == _second_list {
+        result = Comparison::Equal;
+        return result;
+    }
+    for item in _first_list.iter() {
+        if item == &_second_list[i] {
+            result = Comparison::Sublist;
+        } else if result == Comparison::Sublist && item != &_second_list[i] {
+            result = Comparison::Sublist;
+            return result;
+        } else {
+            result = Comparison::Unequal;
+        }
+        i += 1;
+    }
+    if result == Comparison::Sublist { 
+        if _first_list.len() > _second_list.len() {
+            result = Comparison::Superlist
+        }
+    }
+    return result;
 }
